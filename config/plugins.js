5402278377d1a7f55f34c9dd63fddde4b648448b
Marcos_MEDENOU <mmedenou@vippinterstis.com>
2023-11-14 11:35:06 +0100

    @updated

---
+++ config/plugins.js
@@ -0,0 +1,43 @@
+// ./config/plugins.js
+'use strict';
+
+module.exports = {
+  'preview-button': {
+    config: {
+      contentTypes: [
+        {
+          uid: 'api::home.home',
+          published: {
+            url: 'http://localhost:1337',
+          },
+        },
+        {
+          uid: 'api::page.page',
+          draft: {
+            url: 'http://localhost:1337/api/preview',
+            query: {
+              type: 'page',
+              slug: '{slug}',
+            },
+          },
+          published: {
+            url: 'http://localhost:1337/{slug}',
+          },
+        },
+        {
+          uid: 'api::post.post',
+          draft: {
+            url: 'http://localhost:1337/api/preview',
+            query: {
+              type: 'post',
+              slug: '{slug}',
+            },
+          },
+          published: {
+            url: 'http://localhost:1337/blog/{slug}',
+          },
+        },
+      ],
+    },
+  },
+};
